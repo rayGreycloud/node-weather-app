5f8893d088410675dfb857dc552da707add41b0b
rayGreycloud <rayGreycloud@gmail.com>
2016-10-19 16:17:35 -0700

    Revise http request for location data

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5336%20NE%2028th%20Avenue%20Portland',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
