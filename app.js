59d2b41f8ea3b725cc2bf05f9bb957892ae7b674
rayGreycloud <rayGreycloud@gmail.com>
2016-10-20 06:43:44 -0700

    Add yargs for user input

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+})
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5336%20NE%2028th%20Avenue%20Portland',
