5d888da692e729297cf30371a736b016ec65f6f6
rayGreycloud <rayGreycloud@gmail.com>
2016-10-21 12:53:27 -0700

    Add weather search api call

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const request = require('request');
 
 const geocode = require('./geocode/geocode');
 
@@ -15,10 +16,28 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+
+const apiKey = '334bd309c030f89e8fa97201b40e8959';
+
+request({
+    url: `https://api.darksky.net/forecast/334bd309c030f89e8fa97201b40e8959/45.5620183,-122.6365569`,
+    json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to Dark Sky servers.');
+  } else if (response.statusCode === 400 || 404) {
+    console.log('Unable to fetch weather.');
+  } else if (response.statusCode === 200){
+    console.log(`Latitude: ${body.latitude}`);
+    console.log(`Longitude: ${body.longitude}`);
+    console.log(`Current Temp: ${body.currently.temperature}`);
+  } else { console.log(response.statusCode);}
 });
